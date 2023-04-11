2c0193f5ec00f56ed6b92b3957c31e106de55a5e
Andrei V. Goryunov <a@demob.org>
2023-04-11 19:14:18 +0300

    add to HttpFactory helpers for json, text and html

--- src/HttpFactory.php
+++ src/HttpFactory.php
@@ -5,10 +5,14 @@ namespace Directee\Interfaces;
 use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
 
 interface HttpFactory extends
     RequestFactoryInterface,
     ResponseFactoryInterface,
     StreamFactoryInterface
 {
+    public function createJsonResponse($data, int $code = 200, string $reasonPhrase = ''): ResponseInterface;
+    public function createTextResponse($text, int $code = 200, string $reasonPhrase = ''): ResponseInterface;
+    public function createHtmlResponse($html, int $code = 200, string $reasonPhrase = ''): ResponseInterface;
 }
